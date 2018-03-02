4aa399e803f20e99a8dc7c96304c2c817b950d63
bernashetti <bernashetti@192.168.2.120>
2018-03-01 20:01:58 -0500

    New Autonomous is Mostly Coded!
    Added Arrays, Coded 5/8 States, Read Arrays

--- Fahrenheit18/src/org/usfirst/frc/team6882/globals/autoMap.java
+++ Fahrenheit18/src/org/usfirst/frc/team6882/globals/autoMap.java
@@ -5,6 +5,144 @@ public class autoMap {
 	{
 		START, FORWARD, RIGHT, LEFT, SWITCH, SCALE, RESET, FINISH
 	}
+	public State autoPaths[][] = new State[12][10];
+	public double autoArgs[][] = new double[12][10];
 	
-	
+	public autoMap() {
+		autoPaths[0][0] = State.START;
+		autoPaths[0][1] = State.FORWARD;
+		autoArgs[0][1] = 168.0;
+		autoPaths[0][2] = State.FINISH;
+		
+		autoPaths[1][0] = State.START;
+		autoPaths[1][1] = State.FORWARD;
+		autoArgs[1][1] = 122.0;
+		autoPaths[1][2] = State.FINISH;
+		
+		autoPaths[2][0] = State.START;
+		autoPaths[2][1] = State.FORWARD;
+		autoArgs[2][1] = 168.0;
+		autoPaths[2][2] = State.RIGHT;
+		autoArgs[2][2] = 90.0;
+		autoPaths[2][3] = State.SWITCH;
+		autoArgs[2][3] = 15.0;
+		autoPaths[2][4] = State.FINISH;
+		
+		autoPaths[3][0] = State.START;
+		autoPaths[3][1] = State.FORWARD;
+		autoArgs[3][1] = 185.0;
+		autoPaths[3][2] = State.RIGHT;
+		autoArgs[3][2] = 90.0;
+		autoPaths[3][3] = State.FORWARD;
+		autoArgs[3][3] = 166.0;
+		autoPaths[3][4] = State.RIGHT;
+		autoArgs[3][4] = 90.0;
+		autoPaths[3][5] = State.SWITCH;
+		autoArgs[3][5] = 15.0;
+		autoPaths[3][6] = State.FINISH;
+		
+		autoPaths[4][0] = State.START;
+		autoPaths[4][1] = State.FORWARD;
+		autoArgs[4][1] = 324.0;
+		autoPaths[4][2] = State.RIGHT;
+		autoArgs[4][2] = 90.0;
+		autoPaths[4][3] = State.SCALE;
+		autoArgs[4][3] = 15.0;
+		autoPaths[4][4] = State.RESET;
+		autoArgs[4][4] = 15.0;
+		autoPaths[4][5] = State.FINISH;
+		
+		autoPaths[5][0] = State.START;
+		autoPaths[5][1] = State.FORWARD;
+		autoArgs[5][1] = 216.0;
+		autoPaths[5][2] = State.RIGHT;
+		autoArgs[5][2] = 90.0;
+		autoPaths[5][3] = State.FORWARD;
+		autoArgs[5][3] = 264.0;
+		autoPaths[5][4] = State.LEFT;
+		autoArgs[5][4] = 90.0;
+		autoPaths[5][5] = State.FORWARD;
+		autoArgs[5][5] = 108.0;
+		autoPaths[5][6] = State.LEFT;
+		autoArgs[5][6] = 90.0;
+		autoPaths[5][7] = State.SCALE;
+		autoArgs[5][7] = 15.0;
+		autoPaths[5][8] = State.RESET;
+		autoArgs[5][8] = 15.0;
+		autoPaths[5][9] = State.FINISH;
+		
+		autoPaths[7][0] = State.START;
+		autoPaths[7][1] = State.FORWARD;
+		autoArgs[7][1] = 168.0;
+		autoPaths[7][2] = State.LEFT;
+		autoArgs[7][2] = 90.0;
+		autoPaths[7][3] = State.SWITCH;
+		autoArgs[7][3] = 15.0;
+		autoPaths[7][4] = State.FINISH;
+		
+		autoPaths[8][0] = State.START;
+		autoPaths[8][1] = State.FORWARD;
+		autoArgs[8][1] = 324.0;
+		autoPaths[8][2] = State.LEFT;
+		autoArgs[8][2] = 90.0;
+		autoPaths[8][3] = State.SCALE;
+		autoArgs[8][3] = 15.0;
+		autoPaths[8][4] = State.RESET;
+		autoArgs[8][4] = 15.0;
+		autoPaths[8][5] = State.FINISH;
+		
+		autoPaths[9][0] = State.START;
+		autoPaths[9][1] = State.FORWARD;
+		autoArgs[9][1] = 185.0;
+		autoPaths[9][2] = State.LEFT;
+		autoArgs[9][2] = 90.0;
+		autoPaths[9][3] = State.FORWARD;
+		autoArgs[9][3] = 166.0;
+		autoPaths[9][4] = State.RIGHT;
+		autoArgs[9][4] = 90.0;
+		autoPaths[9][5] = State.FORWARD;
+		autoArgs[9][5] = 17.0;
+		autoPaths[9][6] = State.RIGHT;
+		autoArgs[9][6] = 90.0;
+		autoPaths[9][7] = State.SCALE;
+		autoArgs[9][7] = 15.0;
+		autoPaths[9][8] = State.RESET;
+		autoArgs[9][8] = 15.0;
+		autoPaths[9][9] = State.FINISH;
+		
+		autoPaths[10][0] = State.START;
+		autoPaths[10][1] = State.FORWARD;
+		autoArgs[10][1] = 89.0;
+		autoPaths[10][2] = State.LEFT;
+		autoArgs[10][2] = 90.0;
+		autoPaths[10][3] = State.FORWARD;
+		autoArgs[10][3] = 132.0;
+		autoPaths[10][4] = State.RIGHT;
+		autoArgs[10][4] = 90.0;
+		autoPaths[10][5] = State.FORWARD;
+		autoArgs[10][5] = 98.0;
+		autoPaths[10][6] = State.RIGHT;
+		autoArgs[10][6] = 90.0;
+		autoPaths[10][7] = State.SWITCH;
+		autoArgs[10][7] = 15.0;
+		autoPaths[10][8] = State.FINISH;
+		
+		autoPaths[11][0] = State.START;
+		autoPaths[11][1] = State.FORWARD;
+		autoArgs[11][1] = 89.0;
+		autoPaths[11][2] = State.RIGHT;
+		autoArgs[11][2] = 90.0;
+		autoPaths[11][3] = State.FORWARD;
+		autoArgs[11][3] = 132.0;
+		autoPaths[11][4] = State.LEFT;
+		autoArgs[11][4] = 90.0;
+		autoPaths[11][5] = State.FORWARD;
+		autoArgs[11][5] = 98.0;
+		autoPaths[11][6] = State.LEFT;
+		autoArgs[11][6] = 90.0;
+		autoPaths[11][7] = State.SWITCH;
+		autoArgs[11][7] = 15.0;
+		autoPaths[11][8] = State.FINISH;
+	}
 }
+
--- Fahrenheit18/src/org/usfirst/frc/team6882/robot/Autonomous.java
+++ Fahrenheit18/src/org/usfirst/frc/team6882/robot/Autonomous.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team6882.robot;
 
 import org.usfirst.frc.team6882.globals.hardware;
 import org.omg.PortableServer.POAManagerPackage.State;
+import org.usfirst.frc.team6882.globals.autoMap;
 import org.usfirst.frc.team6882.globals.constants;
 import edu.wpi.first.wpilibj.Timer;
 
@@ -15,6 +16,11 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class Autonomous {
 	/* Global variables for Autonomous only here */
+	static autoMap autos = new autoMap();
+	static int autoMode = -1;
+	static int currStep = -1;
+	static double arg;
+	static boolean ready = false;
 
 	/**
 	 * User Initialization code for autonomous mode should go here. Will run once
@@ -32,7 +38,164 @@ public class Autonomous {
 	 * periodically at a regular rate while the robot is in autonomous mode. *
 	 */
 	public static void periodic() {
+		//select which autonomous to run
+		if(autoMode < 0)
+		{
+			// for on/simple switch: simple = false; on = true
+			if(!hardware.autoSwitch.get())
+			{
+				if(!hardware.rightPosition.get() || !hardware.leftPosition.get())
+				{
+					autoMode = 0;
+				}
+				else
+				{
+					autoMode = 1;
+				}
+			}
+			else
+			{
+				if(!hardware.leftPosition.get()) //left side!
+				{
+					if(hardware.leftStick.getZ() <= -0.5) //switch!
+					{
+						// switch is on left
+						if(hardware.driverStation.getGameSpecificMessage().charAt(0) == 'L' || hardware.driverStation.getGameSpecificMessage().charAt(0) == 'l')
+						{
+							autoMode = 2;
+						}
+						else // switch is on the right
+						{
+							autoMode = 3;
+						}
+					}
+					else if(hardware.leftStick.getZ() >= 0.5) //scale!
+					{
+						// scale is on left
+						if(hardware.driverStation.getGameSpecificMessage().charAt(1) == 'L' || hardware.driverStation.getGameSpecificMessage().charAt(1) == 'l')
+						{
+							autoMode = 4;
+						}
+						else // scale is on the right
+						{
+							autoMode = 5;
+						}
+					}
+				}
+				else if(!hardware.rightPosition.get()) // right side
+				{
+					if(hardware.leftStick.getZ() <= -0.5) //switch!
+					{
+						// switch is on left
+						if(hardware.driverStation.getGameSpecificMessage().charAt(0) == 'R' || hardware.driverStation.getGameSpecificMessage().charAt(0) == 'r')
+						{
+							autoMode = 6;
+						}
+						else // switch is on the right
+						{
+							autoMode = 7;
+						}
+					}
+					else if(hardware.leftStick.getZ() >= 0.5) //scale!
+					{
+						// scale is on left
+						if(hardware.driverStation.getGameSpecificMessage().charAt(1) == 'R' || hardware.driverStation.getGameSpecificMessage().charAt(1) == 'r')
+						{
+							autoMode = 8;
+						}
+						else // scale is on the right
+						{
+							autoMode = 9;
+						}
+					}
+				}
+				else //center
+				{
+					// switch is on left
+					if(hardware.driverStation.getGameSpecificMessage().charAt(0) == 'L' || hardware.driverStation.getGameSpecificMessage().charAt(0) == 'l')
+					{
+						autoMode = 10;
+					}
+					else // switch is on the right
+					{
+						autoMode = 11;
+					}
+				}
+			}
 		
+			// ready to start at our first step of our desired auto
+			currStep = 0;
+		}
+		else //ready to go through the states as laid out in autos
+		{
+		arg = autos.autoArgs[autoMode][currStep];
+			
+			switch(autos.autoPaths[autoMode][currStep])
+			{
+				case START:
+					
+					break;
+				case FORWARD:
+					if(hardware.driveBase.driveByInches(-0.6, arg))
+					{
+						hardware.driveBase.stop();
+						currStep++;
+					}
+					break;
+				case RIGHT:
+					if(hardware.driveBase.turnDegrees(arg, true, 0.8))
+					{
+						hardware.driveBase.stop();
+						currStep++;
+					}
+					
+					break;
+				case LEFT:
+					if(hardware.driveBase.turnDegrees(arg, false, 0.8))
+					{
+						hardware.driveBase.stop();
+						currStep++;
+					}
+					
+					break;
+				case SCALE:
+					if(!ready && hardware.driveBase.driveByInches(-0.3, arg))
+					{
+						hardware.driveBase.stop();
+						ready = true;
+					}
+					
+					if(!hardware.liftMax.get())
+					{
+						if(ready)
+						{
+							hardware.manipulators.outtake();
+							ready = false;
+							currStep++;
+						}
+					}
+					else
+					{
+						hardware.manipulators.moveLift(-0.7);
+					}
+					
+					
+					break;
+				case SWITCH:
+					
+					break;
+				case RESET:
+					
+					break;
+				case FINISH:
+					hardware.driveBase.stop();
+					hardware.manipulators.moveLift(0.0);
+					hardware.manipulators.stoptake();
+					break;
+				default:
+					break;
+			}
+		}
 	}
 }
 // end class
