151b0725b502b8795efb05b3d39156ec729ad8d3
Josh Luvera <jcluvera@yahoo.com>
2019-04-03 09:46:39 -0400

    Revert "Re-structured file system"
    This reverts commit 20bbed662a916eed01bc0c691bfce020e38f80f8.

---
+++ Fahrenheit18/src/org/usfirst/frc/team6882/globals/hardware.java
@@ -0,0 +1,154 @@
+// ====================================================================
+// FILE NAME: Hardware.java (Team 6882 - Fahrenheit)
+//
+// CREATED ON: 15 January 2018
+// CREATED BY: C. Robison
+// MODIFIED ON:
+// MODIFIED BY:
+// ABSTRACT:
+// This file contains all of the global definitions for the
+// hardware objects in the system
+//
+// NOTE: Please do not release this code without permission from
+// Team 6882.
+// ====================================================================
+package org.usfirst.frc.team6882.globals;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Timer;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+
+import org.usfirst.frc.team6882.interfaces.Transmission;
+import org.usfirst.frc.team6882.interfaces.Manipulators;
+
+// -------------------------------------------------------
+/**
+ * puts all of the hardware declarations into one place. In addition, it makes
+ * them available to both autonomous and teleop.
+ */
+
+public class hardware {
+	// **********************************************************
+	// DIGITAL I/O CLASSES
+	// **********************************************************
+	
+	// ====================================
+	// PWM classes
+	// ====================================
+
+	// ------------------------------------
+	// Spark Classes
+	// ------------------------------------
+	public static Spark leftSpark1 = new Spark(3);
+	public static Spark leftSpark2 = new Spark(2);
+	public static Spark rightSpark1 = new Spark(1);
+	public static Spark rightSpark2 = new Spark(0);
+	
+	public static Spark intakeSparkLeft = new Spark(4);
+	public static Spark intakeSparkRight = new Spark(5);
+	
+	// ====================================
+	// CAN classes
+	// ====================================
+	
+	// ------------------------------------
+	// Talon classes
+	// ------------------------------------
+	public static WPI_TalonSRX liftTalon = new WPI_TalonSRX(0);
+
+	// ====================================
+	// Digital Inputs
+	// ====================================
+	// ------------------------------------
+	// Single and double throw switches
+	// ------------------------------------
+	public static DigitalInput autoSwitch = new DigitalInput(7);
+	
+	//public static DigitalInput leftPosition = new DigitalInput(1);
+	//public static DigitalInput rightPosition = new DigitalInput(2);
+	
+	// ------------------------------------
+	// Encoders
+	// ------------------------------------
+	public static Encoder leftDriveEncoder = new Encoder(4, 3);
+	public static Encoder rightDriveEncoder = new Encoder(6, 5);
+	
+	// ------------------------------------
+	// Limit Switches
+	// ------------------------------------
+	public static DigitalInput liftMax = new DigitalInput(0);
+	public static DigitalInput liftMin = new DigitalInput(8);
+	//public static DigitalInput cubeIn = new DigitalInput(9);
+	
+	//------------------------------------
+	//Timer
+	//------------------------------------
+	public static final Timer autoTimer = new Timer();
+
+	// **********************************************************
+	// SOLENOID I/O CLASSES
+	// ******** **************************************************
+	// ====================================
+	// Compressor class - runs the compressor
+	// ====================================
+
+	// ====================================
+	// Pneumatic Control Module
+	// ====================================
+	
+
+	// ====================================
+	// Solenoids
+	// ====================================
+	// ------------------------------------
+	// Double Solenoids
+	// ------------------------------------
+	// Forward = extended = down or stowed; Backward = retracted = up in working position
+	public static DoubleSolenoid flipper1 = new DoubleSolenoid(1, 0);
+	public static DoubleSolenoid flipper2 = new DoubleSolenoid(3, 2);
+
+	
+	
+
+	// **********************************************************
+	// roboRIO CONNECTIONS CLASSES
+	// **********************************************************
+	// -------------------------------------
+	// Axis/USB Camera class
+	// -------------------------------------
+	public static UsbCamera POVCamera = new UsbCamera("camera", 0);
+
+
+	// **********************************************************
+	// DRIVER STATION CLASSES
+	// **********************************************************
+
+	// ------------------------------------
+	// DriverStations class
+	// ------------------------------------
+	public static DriverStation driverStation = DriverStation.getInstance();
+
+	// ------------------------------------
+	// Joystick classes
+	// ------------------------------------
+	public static Joystick leftStick = new Joystick(0);
+	public static Joystick rightStick = new Joystick(1);
+	public static Joystick gamePad1 = new Joystick(2);
+	
+
+	
+	// **********************************************************
+	// DRIVE AND MANIPULATOR CLASSES
+	// **********************************************************
+	public static Manipulators manipulators = new Manipulators(liftTalon, flipper1, flipper2, intakeSparkLeft, intakeSparkRight);
+	public static Transmission driveBase = new Transmission(leftSpark1, leftSpark2, rightSpark1, rightSpark2, leftDriveEncoder, rightDriveEncoder);
+
+} // end class
---
+++ Fahrenheit18/src/org/usfirst/frc/team6882/interfaces/Transmission.java
@@ -0,0 +1,198 @@
+package org.usfirst.frc.team6882.interfaces;
+
+import org.usfirst.frc.team6882.globals.constants;
+import org.usfirst.frc.team6882.globals.hardware;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+
+public class Transmission {
+
+	private Spark left1, left2, right1, right2;
+	private Encoder leftEnc, rightEnc;
+	private boolean fast = true;
+	private boolean gearButtonState = false;
+	private boolean encReset = true;
+
+	/**
+	 * Creates a transmission for a 6 wheel drop center robot with 2 motors per
+	 * side.
+	 * 
+	 * @param left1
+	 * @param left2
+	 * @param right1
+	 * @param right2
+	 */
+	public Transmission(Spark left1, Spark left2, Spark right1, Spark right2, Encoder leftEnc, Encoder rightEnc) {
+		this.left1 = left1;
+		this.left2 = left2;
+		this.right1 = right1;
+		this.right2 = right2;
+		this.leftEnc = leftEnc;
+		this.rightEnc = rightEnc;
+	}
+
+	/**
+	 * Stop the robot.
+	 */
+	public void stop() {
+		left1.set(0);
+		left2.set(0);
+		right1.set(0);
+		right2.set(0);
+	}
+
+	/**
+	 * Drive the robot.
+	 * 
+	 * @param speedLeft
+	 *            Range of values: -1 (full reverse) to +1 (full forward)
+	 * @param speedRight
+	 *            Range of values: -1 (full reverse) to +1 (full forward)
+	 */
+	public void drive(double speedLeft, double speedRight) {
+		double currentSpeed;
+		
+		// determine speed factor based on current gear
+		if (fast) {
+			currentSpeed = constants.fastSpeedFactor;
+		} else {
+			currentSpeed = constants.slowSpeedFactor;
+		}
+		// set motors to ratio of current gear speed based on given desired speed
+		left1.set(currentSpeed * speedLeft);
+		left2.set(currentSpeed * speedLeft);
+		right1.set(currentSpeed * speedRight);
+		right2.set(currentSpeed * speedRight);
+	}
+	
+	/**
+	 * Drive the robot forward by a variable number of inches
+	 * 
+	 * @param speed - speed the robot should drive forward
+	 * @param inches - desired number of inches to travel
+	 * @return - true if traveled desired distance; false otherwise
+	 */
+	public boolean driveByInches(double speed, double inches) {
+		// doubles to hold speed for left and right side
+		double left = 0.0;
+		double right = 0.0;
+		boolean stop = false;
+		
+		// only reset encoders the first time called per step
+		if(encReset) {
+			encReset = false;
+			leftEnc.reset();
+			rightEnc.reset();
+		}
+		
+//		//drive left side if it has not gone far enough 
+		if(leftEnc.getDistance() < inches && !stop) {
+			left = speed;
+		}
+		else
+		{
+			left = 0.0;
+			stop = true;
+		}
+		
+		//drive right side if it has not gone far enough
+		if(rightEnc.getDistance() < inches && !stop) {
+			right = speed * 1.005;
+//			left = speed;
+		}
+		else
+		{
+			right = 0.0;
+			stop = true;
+		}
+		
+		//call to make motors move 
+		this.drive(left, right);
+		
+		//if robot has gone far enough set encoder to true 
+		if(left == 0 && right == 0) {
+			encReset = true;
+		}
+		
+		//return encReset: true if gone far enough, false otherwise
+		return(encReset);
+	}
+	
+	/**
+	 * Turn the robot about its center line a variable number of degrees
+	 * 
+	 * @param degrees - desired number of degrees to turn
+	 * @param rightTurn - true if turning right; false otherwise
+	 * @param speed - speed to turn
+	 * @return - true when turned far enough; false otherwise
+	 */
+	public boolean turnDegrees(double degrees, boolean rightTurn, double speed) {
+		// variables for storing driving speed on the left and right
+		double left = 0.0;
+		double right = 0.0;
+		
+		// convert from degrees to inches each wheel will travel
+		double inchesToTravel = (degrees - 8.0) * constants.inchesPerDegree;
+		
+		//Reseting the encoder
+		if(encReset) {
+			leftEnc.reset();
+			rightEnc.reset();
+			encReset = false;
+		}
+		
+		// if turning right, left wheel goes forward and right wheel goes backwards
+		if (rightTurn){
+			//Left forward
+			if(leftEnc.getDistance() < inchesToTravel) {
+				left = -speed;
+			}
+			
+			//Right backward
+			if(rightEnc.getDistance() > -(inchesToTravel)) {
+				right = speed;
+			}
+		}
+		// turning left, so left wheel goes backwards and right wheel goes forwards
+		else {
+			//Left backward
+			if(leftEnc.getDistance() > -(inchesToTravel)) {
+				left = speed;
+			}
+			
+			//Right forward
+			if(rightEnc.getDistance() < inchesToTravel) {
+				right = -speed;
+			}
+		}
+		
+
+		// Move the wheels
+		this.drive(left, right);
+		
+		//If turned far enough, prepare for next call via encReset
+		if(left == 0 && right == 0) {
+			encReset = true;
+		}
+		
+		// return state of resetEnc - true if turned far enough; false if turned not far enough
+		return(encReset);
+	}
+		
+	/**
+	 * The robot will change gears.
+	 * 
+	 * @param button
+	 *            - indicates the state of the button
+	 */
+	public void changeGear(boolean button) {
+		// if the button is pressed and the gearButtonState indicates that it has not been changed, change the gear
+		if (button && !gearButtonState) {
+			fast = !fast;
+		}
+
+		// set the gearButtonState to match the button state
+		gearButtonState = button;
+	}
+}
