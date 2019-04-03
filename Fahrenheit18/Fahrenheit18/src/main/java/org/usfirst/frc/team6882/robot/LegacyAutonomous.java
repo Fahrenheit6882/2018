2fbc136248fd76ed45ff257fcc313010a31a8285
Josh Luvera <jcluvera@yahoo.com>
2019-04-02 22:09:18 -0400

    Conversion from an Eclipse workspace to a VSCode.

---
+++ Fahrenheit18/Fahrenheit18/src/main/java/org/usfirst/frc/team6882/robot/LegacyAutonomous.java
@@ -0,0 +1,297 @@
+package org.usfirst.frc.team6882.robot;
+
+import org.usfirst.frc.team6882.globals.hardware;
+import org.omg.PortableServer.POAManagerPackage.State;
+import org.usfirst.frc.team6882.globals.constants;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * An Autonomous class. This class uses state machines in order to periodically
+ * execute instructions during the Autonomous period.
+ * 
+ * This class contains all of the user code for the Autonomous part of the
+ * match, namely, the Init and Periodic code
+ */
+
+public class LegacyAutonomous {
+	/* Global variables for Autonomous only here */
+	static Timer autoTimer;
+	static boolean started = false;
+	// Reading game data and storing as char
+	static char ourSwitch;
+	static char Scale;
+	static char startPosition; 
+
+	// how far to travel in simple auto
+	static double travelDistance = 0;
+
+	/**
+	 * User Initialization code for autonomous mode should go here. Will run once
+	 * when the autonomous first starts, and will be followed immediately by
+	 * periodic().
+	 */
+	public static void init() {
+		// initialize timer
+//		autoTimer = new Timer();
+
+		// read game data
+		ourSwitch = ' ';
+		Scale = ' ';
+		
+		//TODO read the switch
+		
+//		if(!hardware.leftPosition.get())
+//		{
+//			startPosition = 'L';
+//		}
+//		else if (!hardware.rightPosition.get())
+//		{
+//			startPosition = 'R';
+//		}
+//		else
+//		{
+//			startPosition = 'C';
+//		}
+		
+	} // end Init
+
+	public static enum State {
+		START, LIFTSWITCH, CENTERAPPROACHSWITCH, FIELDSWITCHTURN, FORWARD, PAUSE, RESETLIFT, DROPBLOCK, REVERSE, REVERSEDRIVE, EXCHANGEGIVE, AFTERREVERSETURN, APPROACHLASTEXCHANGETURN, RESET, BLOCK, STOP, FINISH
+	}
+
+	public static State autoState = State.START;
+	
+	private static boolean lifted = false;
+	private static int cnt = 0;
+	private static int maxCnt = 10;
+
+	/**
+	 * User Periodic code for autonomous mode should go here. Will be called
+	 * periodically at a regular rate while the robot is in autonomous mode. *
+	 */
+	public static void periodic() {
+		// if Autonomous is not disabled (i.e. Autonomous is enabled) do some things
+		//TODO Put physical switch read back into the if statement
+		if (hardware.autoSwitch.get()) {
+			if(autoState != State.FINISH)
+			{
+				System.out.println(autoState.toString());
+			}
+			
+			switch (autoState) {
+				case START:
+					hardware.driveBase.stop();
+					String rawData = hardware.driverStation.getGameSpecificMessage();
+					
+					if(rawData != "") {
+						ourSwitch = rawData.charAt(0);
+						Scale = rawData.charAt(1);
+					}
+					
+					if(cnt < maxCnt)
+					{
+						//intake cube
+						hardware.manipulators.intake();
+						hardware.manipulators.moveLift(0);
+						cnt++;
+					}
+					else
+					{
+						autoState = State.FORWARD;
+//						autoState = State.FINISH;
+						cnt = 0;
+					}
+
+					if(!lifted && cnt == 2)
+					{
+						//putting manipulator in upright position
+						hardware.manipulators.pullUpManipulator(true);
+						lifted = true;
+					}
+					
+					break;
+				
+				case FORWARD:
+					//turn off intake first
+					hardware.manipulators.intake();
+					
+					//if L or R, drive forward 168 inches before changing states
+					switch(startPosition)
+					{
+						case 'r':
+						case 'R':
+						case 'l':
+						case 'L':
+							if (hardware.driveBase.driveByInches(-0.6, (168 - constants.stoppingDistance)))
+							{
+								hardware.driveBase.stop();
+								
+								cnt = 0;
+								
+								// TODO add logic for when to turn towards the switch
+								autoState = State.PAUSE;
+//								autoState = State.FINISH;
+							}	
+							break;
+						case 'c':
+						case 'C':
+							if (hardware.driveBase.driveByInches(-0.6, 30))
+							{
+								hardware.driveBase.stop();
+								
+								autoState = State.FINISH;
+							}
+							break;
+						
+					}
+					
+					break;
+				case PAUSE:
+				{
+					if (cnt < 10)
+					{
+						hardware.driveBase.stop();
+						cnt++;
+					}
+					else
+					{
+//						hardware.leftDriveEncoder.reset();
+						hardware.rightDriveEncoder.reset();
+						autoState = State.FIELDSWITCHTURN;
+//						autoState = State.FINISH;
+						cnt = 0;
+					}
+				}
+				case FIELDSWITCHTURN:
+					switch (startPosition)
+					{
+						case 'r':
+						case 'R':
+							if(hardware.driveBase.turnDegrees(65, false, 0.5))
+							{
+//								autoState = State.FINISH;
+								autoState = State.LIFTSWITCH;
+							}
+							break;
+						case 'l':
+						case 'L':
+							if(hardware.driveBase.turnDegrees(95, true, 0.5))
+							{
+								autoState = State.FINISH;
+								autoState = State.LIFTSWITCH;
+							}
+							break;
+					}
+					break; 
+					
+				case LIFTSWITCH:
+					//TODO LIFTSWITCH need to write code for lifting block
+					
+					hardware.manipulators.moveLift(-1.0);
+					//change tempTick to the ticks on the encoders 19 inches is the goal.
+					if(hardware.driveBase.driveByInches(-0.3, 15 - constants.stoppingDistance)) {
+						hardware.manipulators.moveLift(0);
+						autoState = State.DROPBLOCK;
+//						autoState = State.FINISH;
+					}
+					break;
+					
+				case DROPBLOCK:
+					// TODO code DROPBLOCK
+					if(cnt < maxCnt)
+					{
+						hardware.manipulators.outtake();
+						cnt++;
+					}
+					else
+					{
+						cnt = 0;
+						hardware.manipulators.stoptake();
+						autoState = State.FINISH;
+					}
+					break;
+					
+				case RESETLIFT:
+					// TODO back up and lower the lift
+					hardware.driveBase.driveByInches(-0.3, 39.25);
+					//Need to reset lift motors at the same time
+					hardware.liftTalon.set(-1);
+					break;
+					
+				case REVERSE:
+					if(hardware.driveBase.driveByInches(-1, 80)) {
+						autoState = State.AFTERREVERSETURN;
+					}
+					break;
+									
+				case AFTERREVERSETURN:
+					if(startPosition == 'R' & ourSwitch == 'R') {
+						hardware.driveBase.stop();
+						autoState = State.APPROACHLASTEXCHANGETURN;
+					}
+					
+					break;
+
+				case REVERSEDRIVE:
+					hardware.driveBase.drive (-1, -1);
+					
+					//End curly bracket of condition here
+					//The state: EXCHANGEGIVE will make sure that the arm is lowered and insert the cube into the exchange
+					break;
+					
+				case FINISH:
+					hardware.driveBase.stop();
+					hardware.manipulators.moveLift(0);
+					break;
+	
+				default:
+					break;
+			}
+		}
+		else
+		{
+			switch (autoState) 
+			{
+				case START:
+					if(!lifted)
+					{
+						//putting manipulator in upright position
+						hardware.manipulators.pullUpManipulator(true);
+						lifted = true;
+						if (startPosition == 'C' || startPosition == 'c')
+						{
+							travelDistance = 95;
+						}
+						else
+						{
+							travelDistance = 168;
+						}
+					}
+					
+					break;
+				
+				case FORWARD:
+					if (hardware.driveBase.driveByInches(-0.6, (travelDistance - constants.stoppingDistance)))
+						{
+							hardware.driveBase.stop();
+							
+							cnt = 0;
+							
+							// TODO add logic for when to turn towards the switch
+							autoState = State.FINISH;
+						}						
+					break;
+					
+				case FINISH:
+					hardware.driveBase.stop();
+					hardware.manipulators.moveLift(0);
+					break;
+	
+				default:
+					break;				
+				
+			}
+		}
+	}
+}
+// end class
---
+++ Fahrenheit18/Fahrenheit18/src/main/java/org/usfirst/frc/team6882/robot/Robot.java
@@ -0,0 +1,431 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+// ====================================================================
+// FILE NAME: Fahrenheit18.java (Team 6882 - Fahrenheit Robotics)
+//
+// CREATED ON: 15 January 2018
+// CREATED BY: C. Robison
+// MODIFIED ON:
+// MODIFIED BY:
+// ABSTRACT:
+// This file is where almost all code for the robot will be
+// written. All of these functions are functions that should
+// override methods in the base class (IterativeRobot). The
+// functions are as follows:
+//
+// -----------------------------------------------------
+// autonomousInit() - Initialization code for autonomous mode
+// should go here. Will be called each time the robot enters
+// autonomous mode.
+//
+// disabledInit() - Initialization code for disabled mode should
+// go here. This function will be called one time when the
+// robot first enters disabled mode.
+//
+// robotInit() - Robot-wide initialization code should go here.
+// It will be called exactly 1 time.
+//
+// teleopInit() - Initialization code for teleop mode should go here.
+// Will be called each time the robot enters teleop mode.
+// -----------------------------------------------------
+// autonomousPeriodic() - Periodic code for autonomous mode should
+// go here. Will be called periodically at a regular rate while
+// the robot is in autonomous mode.
+//
+// disabledPeriodic() - Periodic code for disabled mode should go here.
+// Will be called periodically at a regular rate while the robot
+// is in disabled mode.
+//
+// teleopPeriodic() - Periodic code for teleop mode should go here.
+// Will be called periodically at a regular rate while the robot
+// is in teleop mode.
+// -----------------------------------------------------
+// autonomousContinuous() - Continuous code for autonomous mode should
+// go here. Will be called repeatedly as frequently as possible
+// while the robot is in autonomous mode.
+//
+// disabledContinuous() - Continuous code for disabled mode should go
+// here. Will be called repeatedly as frequently as possible while
+// the robot is in disabled mode.
+//
+// teleopContinuous() - Continuous code for teleop mode should go here.
+// Will be called repeatedly as frequently as possible while the
+// robot is in teleop mode.
+// -----------------------------------------------------
+// Other functions not normally used
+// startCompetition() - This function is a replacement for the WPI
+// supplied 'main loop'. This should not normally be written or
+// used.
+// -----------------------------------------------------
+//
+// NOTE: Please do not release this code without permission from
+// Team 6882.
+// ====================================================================
+
+package org.usfirst.frc.team6882.robot;
+
+import org.usfirst.frc.team6882.globals.constants;
+import org.usfirst.frc.team6882.globals.hardware;
+
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+
+// -------------------------------------------------------
+
+/**
+ * declares all the code necessary to extend the IterativeRobot class. These are
+ * all the methods needed to run the robot during a match
+ */
+public class Robot extends IterativeRobot {
+	// =================================================
+	// private data for the class
+	// =================================================
+	// -------------------------------------------------------
+
+	/**
+	 * Initialization code for autonomous mode should go here. Will be called once
+	 * when the robot enters autonomous mode. *
+	 */
+	@Override
+	public void autonomousInit() {
+		// ---------------------------------------
+		// start setup - tell the user we are beginning
+		// setup
+		// ---------------------------------------
+		System.out.println("Started AutonousInit().");
+
+		// =========================================================
+		// User code goes below here
+		// =========================================================
+		Autonomous.init();
+		// =========================================================
+		// User code goes above here
+		// =========================================================
+		// ---------------------------------------
+		// done setup - tell the user we are complete
+		// setup
+		// ---------------------------------------
+		System.out.println("Completed AutonousInit().");
+	} // end autonomousInit
+
+	// -------------------------------------------------------
+	/**
+	 * Non-User Periodic code for autonomous mode should go here. Will be called
+	 * periodically at a regular rate while the robot is in autonomous mode. This in
+	 * turn calls the Autonomous class's Periodic function, which is where the user
+	 * code should be placed.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2011 -------------------------------------------------------
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		// System.out.println("Started AutonousPeriodic().");
+		// =========================================================
+		// User code goes below here
+		// =========================================================
+		Autonomous.periodic();
+		// =========================================================
+		// User code goes above here
+		// =========================================================
+
+	}// end autonomousPeriodic
+		// -------------------------------------------------------
+
+	/**
+	 * Initialization code for disabled mode should go here. Will be called once
+	 * when the robot enters disabled mode.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2011 -------------------------------------------------------
+	 */
+	@Override
+	public void disabledInit() {
+		// ---------------------------------------
+		// start setup - tell the user we are beginning
+		// setup
+		// ---------------------------------------
+		System.out.println("Started DisabledInit().");
+		// =========================================================
+		// User code goes below here
+		// =========================================================
+
+		// =========================================================
+		// User code goes above here
+		// =========================================================
+		// ---------------------------------------
+		// done setup - tell the user we are complete
+		// setup
+		// ---------------------------------------
+		System.out.println("Completed DisabledInit().");
+	} // end disabledInit
+
+	// -------------------------------------------------------
+	/**
+	 * Periodic code for disabled mode should go here. Will be called periodically
+	 * at a regular rate while the robot is in disabled mode. Code that can be
+	 * "triggered" by a joystick button can go here. This can set up configuration
+	 * things at the driver's station for instance before a match.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2011 -------------------------------------------------------
+	 */
+	@Override
+	public void disabledPeriodic() {
+		// -------------------------------------
+		// Watch dog code used to go here.
+		// -------------------------------------
+		// =========================================================
+		// User code goes below here
+		// =========================================================
+
+		// =========================================================
+		// User code goes above here
+		// =========================================================
+	} // end disabledPeriodic
+		// -------------------------------------------------------
+
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code for the robot.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2011 -------------------------------------------------------
+	 */
+	@Override
+	public void robotInit() {
+		// ---------------------------------------
+		// start setup - tell the user we are beginning
+		// setup
+		// ---------------------------------------
+		System.out.println("Started robotInit()");
+
+		// =========================================================
+		// User code goes below here
+		
+		// Invert motor controllers for right side of drive so positive is forward
+		hardware.leftSpark1.setInverted(true);
+		hardware.leftSpark2.setInverted(true);
+		
+		// Invert motor controller for one side of intake
+		hardware.intakeSparkRight.setInverted(true);
+		
+		// Set drive encoders distance per pulse
+		hardware.leftDriveEncoder.setDistancePerPulse(constants.inchesPerPulseLeft);
+		hardware.rightDriveEncoder.setDistancePerPulse(constants.inchesPerPulseRight);
+
+		// User code goes above here
+		// =========================================================
+		// ---------------------------------------
+		// done setup - tell the user we are complete
+		// setup
+		// ---------------------------------------
+		System.out.println("Fahrenheit-18 is started.  All hardware items created.");
+	} // end
+		// robotInit
+
+	// -------------------------------------------------------
+	/**
+	 * Non-User initialization code for teleop mode should go here. Will be called
+	 * once when the robot enters teleop mode, and will call the Teleop class's Init
+	 * function, where the User code should be placed.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2011 -------------------------------------------------------
+	 */
+	@Override
+	public void teleopInit() {
+		// ---------------------------------------
+		// start setup - tell the user we are beginning
+		// setup
+		// ---------------------------------------
+		System.out.println("Started teleopInit().");
+		// =========================================================
+		// User code goes below here
+
+		Teleop.init();
+
+		// User code goes above here
+		// =========================================================
+		// ---------------------------------------
+		// done setup - tell the user we are complete
+		// setup
+		// ---------------------------------------
+		System.out.println("Completed TeleopInit().");
+	} // end teleopInit
+
+	// -------------------------------------------------------
+	/**
+	 * Non-User Periodic code for teleop mode should go here. Will be called
+	 * periodically at a regular rate while the robot is in teleop mode, and will in
+	 * turn call the Teleop class's Periodic function.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2011 -------------------------------------------------------
+	 */
+	@Override
+	public void teleopPeriodic() {
+		// -------------------------------------
+		// Call the Teleop class's Periodic function,
+		// which contains the user code.
+		// -------------------------------------
+
+		// =========================================================
+		// User code goes below here
+
+		Teleop.periodic();
+
+		// User code goes above here
+		// =========================================================
+
+	} // end teleopPeriodic
+		// -------------------------------------------------------
+
+	/**
+	 * Initialization code for test mode should go here. Will be called once when
+	 * the robot enters test mode.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2015 -------------------------------------------------------
+	 */
+	@Override
+	public void testInit() {
+		// =========================================================
+		// User code goes below here
+		// =========================================================
+//		boolean leftPos = hardware.leftPosition.get();
+//		boolean rightPos = hardware.rightPosition.get();
+//		
+//		System.out.println("Left position = " + !leftPos);
+//		System.out.println("Right position = " + !rightPos);
+		
+		//testing encoders
+		//hardware.leftDriveEncoder.reset();
+		//hardware.rightDriveEncoder.reset();
+		
+		//System.out.println("Encoders reset. Left: " + hardware.leftDriveEncoder.getDistance() + " Right: " + hardware.rightDriveEncoder.getDistance());
+		
+		//testing auto under here
+		
+		// =========================================================
+		// User code goes above here
+		// =========================================================
+
+	} // end testInit
+		// -------------------------------------------------------
+
+	/**
+	 * Periodic code for test mode should go here. Will be called periodically at a
+	 * regular rate while the robot is in test mode.
+	 *
+	 * @author Bob Brown
+	 * @written Jan 2, 2015 -------------------------------------------------------
+	 */
+	@Override
+	public void testPeriodic() {
+		// =========================================================
+		// User code goes below here
+		// =========================================================
+		
+//		//Test Switches
+		if(hardware.leftStick.getRawButtonPressed(7))
+		{
+			System.out.println("disable = "  + hardware.autoSwitch.get());
+//			System.out.println("Left = " + hardware.leftPosition.get());
+//			System.out.println("Right = " + hardware.rightPosition.get());
+			System.out.println("liftMax = " + hardware.liftMax.get());
+			System.out.println("liftMin = " + hardware.liftMin.get());
+			hardware.rightDriveEncoder.reset();
+			hardware.leftDriveEncoder.reset();
+		}
+//		
+		
+		//Test drive by inches and turn by degrees
+		
+//		if (hardware.driveBase.driveByInches(-0.3, 168))
+//		{
+//			hardware.rightDriveEncoder.reset();
+//			hardware.driveBase.stop();
+//		}
+		if(hardware.leftStick.getRawButtonPressed(6))
+		{
+			tempLeft = hardware.leftDriveEncoder.get();
+			tempRight = hardware.rightDriveEncoder.get();
+		
+			temp++;
+			System.out.println("Temp = " + temp + " Left = " + tempLeft + " Right = " + tempRight);
+		}
+//		
+		
+//		if(!d)
+//		{
+//			if(hardware.driveBase.driveByInches(-0.4, 30))
+//			{
+//				System.out.println("30 inches done.");
+//				d = true;
+//			}
+//		}
+//		
+//		if(!d)
+//		{
+//			if(hardware.driveBase.turnDegrees(90, true, 0.5))
+//			{
+//				System.out.println("90 degress done.");
+//				d = true;
+//			}
+//		}		
+//		
+		//Test button outputs
+//		boolean btn1 = hardware.gamePad1.getRawButtonPressed(1);
+//		boolean btn2 = hardware.gamePad1.getRawButtonPressed(2);
+//		boolean btn3 = hardware.gamePad1.getRawButtonPressed(3);
+//		boolean btn4 = hardware.gamePad1.getRawButtonPressed(4);
+//		boolean btn5 = hardware.gamePad1.getRawButtonPressed(5);
+//		boolean btn6 = hardware.gamePad1.getRawButtonPressed(6);
+//		boolean btn7 = hardware.gamePad1.getRawButtonPressed(7);
+//		boolean btn8 = hardware.gamePad1.getRawButtonPressed(8);
+//		boolean btn9 = hardware.gamePad1.getRawButtonPressed(9);
+//		boolean btn10 = hardware.gamePad1.getRawButtonPressed(10);
+//		boolean btn11 = hardware.gamePad1.getRawButtonPressed(11);
+//		boolean btn12 = hardware.gamePad1.getRawButtonPressed(12);
+//		boolean btn13 = hardware.gamePad1.getRawButtonPressed(13);
+//		boolean btn14 = hardware.gamePad1.getRawButtonPressed(14);
+//		boolean btn15 = hardware.gamePad1.getRawButtonPressed(15);
+//		boolean btn16 = hardware.gamePad1.getRawButtonPressed(16);
+//		
+//		if(btn1 || btn2 || btn3 || btn4 || btn5 || btn6 || btn7 || btn8 || btn9 || btn10 || btn11 || btn12 || btn13 || btn14 || btn15 || btn16)
+//		{
+//			System.out.println("Btn 1: " + btn1 + " Btn 2: " + btn2 + " Btn 3: " + btn3 + " Btn 4: " + btn4);
+//			System.out.println("Btn 5: " + btn5 + " Btn 6: " + btn6 + " Btn 7: " + btn7 + " Btn 8: " + btn8);
+//			System.out.println("Btn 9: " + btn9 + " Btn 10: " + btn10 + " Btn 11: " + btn11 + " Btn 12: " + btn12);
+//			System.out.println("Btn 13: " + btn13 + " Btn 14: " + btn14 + " Btn 15: " + btn15 + " Btn 16: " + btn16);
+//		}
+		
+		
+		// =========================================================
+		// User code goes above here
+		// =========================================================
+
+	} // end testPeriodic
+
+	// ==========================================
+	// GLOBAL TEST VARIABLES
+	// ==========================================
+	int temp = 0;
+	double tempLeft = 0;
+	double tempRight = 0;
+	boolean d = false;
+
+} // end class
